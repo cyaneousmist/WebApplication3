5c5baa23687dda4597c4f09cf7652c816eed2483
tzuyen <lin.tzu.yen.1342@gmail.com>
2024-02-01 06:09:57 +0800

    some updates

--- WebApplication3/ViewModels/Register.cs
+++ WebApplication3/ViewModels/Register.cs
@@ -5,8 +5,33 @@ namespace WebApplication3.ViewModels
 {
     public class Register
     {
+		[Required]
+		[DataType(DataType.Text)]
+		[StringLength(50, MinimumLength = 2)]
+		public string FirstName { get; set; }
+		[Required]
+		[DataType(DataType.Text)]
+		[StringLength(50, MinimumLength = 2)]
+		public string LastName { get; set; }
 
-        [Required]
+		[Required]
+		[DataType(DataType.CreditCard)]
+		public string CreditCard { get; set; }
+
+
+		[Required]
+		[DataType(DataType.PhoneNumber)]
+		public string PhoneNumber { get; set; }
+
+		[Required]
+		[DataType(DataType.Text)]
+		public string BillingAddress { get; set; }
+
+		[Required]
+		[DataType(DataType.Text)]
+		public string ShippingAddress { get; set; }
+
+		[Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
@@ -19,6 +44,9 @@ namespace WebApplication3.ViewModels
         [Compare(nameof(Password), ErrorMessage = "Password and confirmation password does not match")]
         public string ConfirmPassword { get; set; }
 
+		[DataType(DataType.Upload)]
+		public IFormFile Photo { get; set; }
 
-    }
+
+	}
 }
